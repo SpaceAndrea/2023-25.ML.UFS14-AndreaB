b4b62d7570bc909a53176692e4c277fd2eda9bb0
Hypnos999 <flama2204@gmail.com>
2024-10-18 15:32:18 +0000

    ddd
    dd

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import os
+import csv
 
+input_dir = os.environ['SM_INPUT_DIR']
+input_text = ''
+
+print('###### opening train')
+with open(input_dir + "/data/training/train.csv", 'r') as fp:
+    # lines = len(fp.readlines())
+    # print('Total Number of lines:', lines)
+    input_text = str(len(fp.readlines()))
+
+print('###### closed train')
+
+
+print('###### opening model')
 model_dir = os.environ['SM_MODEL_DIR']
-
-with open(model_dir + '/output.txt', 'w') as f:
-    f.write('Ciao')
+with open(model_dir + '/model.txt', 'w') as f:
+    f.write('model cio')
+print('###### closing model')
     
 output_dir = os.environ['SM_OUTPUT_DIR']
 with open(output_dir + '/output.txt', 'w') as f:
-    f.write('Ciao')
+    f.write('Ciao output')
+with open(output_dir + '/output_csv.txt', 'w') as f:
+    f.write(input_text)
+    
