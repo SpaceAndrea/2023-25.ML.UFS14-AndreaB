5bfc2a46dde351cbea7ffa279adbb2e6d72912b0
SpaceAndrea <business.andreabianchini@gmail.com>
2024-11-17 22:14:08 +0000

    Unzip

--- Esame/inference.py
+++ Esame/inference.py
@@ -1,37 +1,58 @@
 import logging
 import json
-import glob
-import sys
-from os import environ
-from flask import Flask
+import os
+from flask import Flask, request
 from keras import models
 import numpy as np
-from flask import request
-
+import tensorflow as tf
+import pickle
+from PIL import Image
+import io
 
 logging.debug('Init a Flask app')
 app = Flask(__name__)
 
+# Carica il modello e la mappatura delle classi
+model_dir = os.environ.get('SM_MODEL_DIR', '/opt/ml/model')
+print(f"######## La model dir è: {model_dir}")
+model = models.load_model(f"{model_dir}/card_model.h5")
 
-def doit(lunghezza, diametro):
-    
-    model_dir = environ['SM_MODEL_DIR']
-    print(f"######## La model dir è: {model_dir}")
-    model = models.load_model(f"{model_dir}/abalone_model.keras")
-    predict_input = np.array([
-        [lunghezza,diametro,0.125,0.5095,0.2165,0.1125,0.165,9]
-    ])
-    predict_result = model.predict(predict_input)
+with open(f"{model_dir}/class_indices.pkl", 'rb') as f:
+    class_indices = pickle.load(f)
+labels = dict((v, k) for k, v in class_indices.items())
 
-    return json.dumps({
-        "inputs": predict_input.tolist(),
-        "predict_result": predict_result.tolist()
-    })
+def preprocess_image(image_bytes):
+    image_size = (224, 224)
+    img = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+    img = img.resize(image_size)
+    x = np.array(img)
+    x = x / 255.0
+    x = np.expand_dims(x, axis=0)
+    return x
 
-@app.route('/ping')
+@app.route('/ping', methods=['GET', 'POST'])
 def ping():
-    logging.debug('Hello from route /ping')
-    lunghezza = request.args.get('lunghezza')
-    diametro = request.args.get('diametro')
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return "Nessun file caricato", 400
+        file = request.files['file']
+        image_bytes = file.read()
+        x = preprocess_image(image_bytes)
+        
+        # Predizione
+        predictions = model.predict(x)
+        predicted_class = np.argmax(predictions, axis=1)
+        predicted_label = labels[predicted_class[0]]
+        
+        # Estrai valore e seme
+        valore, seme = predicted_label.split('_')
+        
+        return json.dumps({
+            "valore": valore,
+            "seme": seme
+        })
+    else:
+        return "Per favore, invia un'immagine via POST."
 
-    return doit(float(lunghezza), float(diametro))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
